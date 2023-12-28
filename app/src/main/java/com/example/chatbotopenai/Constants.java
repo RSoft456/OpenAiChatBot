3a99e887d3a88870c025ff23115cea73a2c9a782
ramsha456 <ramshamehmood.tbox@gmail.com>
2023-12-28 12:48:34 +0500

    some pending files

---
+++ app/src/main/java/com/example/chatbotopenai/Constants.java
@@ -0,0 +1,12 @@
+package com.example.chatbotopenai;
+
+public class Constants {
+    private static String BASE_URL = "https://api.openai.com/v1/";
+    private static String API_KEY = "YOUR_GENERATED_API_KEY";
+    public static String getBASE_URL() {
+        return BASE_URL;
+    }
+    public static String getAPI_KEY() {
+        return API_KEY;
+    }
+}
